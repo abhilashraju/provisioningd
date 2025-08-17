45b54d20d34463af3fb21022c5a00c1654b0c14f
Abhilash Raju <abhilash.kollam@gmail.com>
2025-08-17 15:15:39 +0000

    Adding spdm watcher

--- src/provisioningd.cpp
+++ src/provisioningd.cpp
@@ -1,5 +1,6 @@
 #include "bmcwatcher.hpp"
 #include "provisioning_object.hpp"
+#include "spdmwatcher.hpp"
 
 #include <unistd.h>
 
@@ -13,25 +14,23 @@
 using namespace reactor;
 inline std::string trusStorePath()
 {
-    return std::format("{}etc/ssl/certs/ca.pem", "/tmp/3273986/");
+    return std::format("{}etc/ssl/certs/ca.pem", "/tmp/20057/");
 }
 inline std::string ENTITY_CLIENT_CERT_PATH()
 {
-    return std::format("{}etc/ssl/certs/https/client_cert.pem",
-                       "/tmp/3273986/");
+    return std::format("{}etc/ssl/certs/https/client_cert.pem", "/tmp/20057/");
 }
 inline std::string CLIENT_PKEY_PATH()
 {
-    return std::format("{}etc/ssl/private/client_pkey.pem", "/tmp/3273986/");
+    return std::format("{}etc/ssl/private/client_pkey.pem", "/tmp/20057/");
 }
 inline std::string ENTITY_SERVER_CERT_PATH()
 {
-    return std::format("{}etc/ssl/certs/https/server_cert.pem",
-                       "/tmp/3273986/");
+    return std::format("{}etc/ssl/certs/https/server_cert.pem", "/tmp/20057/");
 }
 inline std::string SERVER_PKEY_PATH()
 {
-    return std::format("{}etc/ssl/private/server_pkey.pem", "/tmp/3273986/");
+    return std::format("{}etc/ssl/private/server_pkey.pem", "/tmp/20057/");
 }
 
 net::awaitable<void> tryConnect(net::io_context& io_context,
@@ -92,72 +91,11 @@ net::awaitable<void> tryConnect(net::io_context& io_context,
         }
     }
 }
-static constexpr auto SPDM_SVC = "xyz.openbmc_project.spdm";
-static constexpr auto SPDM_PATH = "/xyz/openbmc_project/spdm/device1";
-static constexpr auto SPDM_INTF = "xyz.openbmc_project.SpdmDevice";
-using PropertyWatchType = bool;
-AwaitableResult<PropertyWatchType> propertyWatch(
-    sdbusplus::asio::connection& conn, const std::string& path,
-    const std::string& interface, const std::string& property)
-{
-    std::unique_ptr<sdbusplus::bus::match::match> match;
-    auto h = make_awaitable_handler<PropertyWatchType>([path, interface,
-                                                        property, &match,
-                                                        &conn](auto promise) {
-        std::string matchRule = sdbusplus::bus::match::rules::propertiesChanged(
-            path.c_str(), interface.c_str());
-        auto promise_ptr =
-            std::make_shared<decltype(promise)>(std::move(promise));
-        auto propcallback = [property,
-                             promise_ptr](sdbusplus::message::message& msg) {
-            std::string interfaceName;
-            std::map<std::string, std::variant<PropertyWatchType>>
-                changedProperties;
-            std::vector<std::string> invalidatedProperties;
 
-            msg.read(interfaceName, changedProperties, invalidatedProperties);
-
-            LOG_INFO("Properties changed on interface: {}", interfaceName);
-
-            changedProperties | std::ranges::views::filter([&](const auto& p) {
-                return p.first == property;
-            });
-            if (changedProperties.empty())
-            {
-                LOG_ERROR("Property {} not found in changed properties",
-                          property);
-                promise_ptr->setValues(
-                    boost::system::error_code{
-                        boost::system::errc::make_error_code(
-                            boost::system::errc::no_such_file_or_directory)},
-                    PropertyWatchType{});
-                return;
-            }
-            auto it = changedProperties.begin();
-            if (!std::holds_alternative<PropertyWatchType>(it->second))
-            {
-                LOG_ERROR("Property {} is not of type string", property);
-                promise_ptr->setValues(
-                    boost::system::error_code{
-                        boost::system::errc::make_error_code(
-                            boost::system::errc::invalid_argument)},
-                    PropertyWatchType{});
-                return;
-            }
-            auto result = std::get<PropertyWatchType>(it->second);
-            LOG_DEBUG("Property {} changed: {}", property, result);
-
-            promise_ptr->setValues(boost::system::error_code{},
-                                   std::move(result));
-        };
-        match = std::make_unique<sdbusplus::bus::match::match>(
-            conn, matchRule.c_str(), std::move(propcallback));
-    });
-    co_return co_await h();
-}
-net::awaitable<void> startSpdm(
-    sdbusplus::asio::connection& conn, net::io_context& io_context,
-    const std::string& ip, short port, ProvisioningController& controller)
+net::awaitable<void> startSpdm(sdbusplus::asio::connection& conn,
+                               std::shared_ptr<SpdmWatcher> watcher,
+                               net::io_context& ioc, const std::string& ip,
+                               short port, ProvisioningController& controller)
 {
     // This method would start the SPDM provisioning process.
     // Implementation would depend on the specific requirements.
@@ -167,72 +105,90 @@ net::awaitable<void> startSpdm(
     if (ec)
     {
         LOG_ERROR("Failed to start spdm: {}", ec.message());
-        co_return;
-    }
-    auto [ec2, property] =
-        co_await propertyWatch(conn, SPDM_PATH, SPDM_INTF, "Status");
-
-    if (ec2)
-    {
-        LOG_ERROR("Failed to watch property: {}", ec2.message());
-        co_return;
-    }
-    if (property)
-    {
-        co_await tryConnect(io_context, ip, port, controller);
     }
 
-    co_return;
+    bool prop = co_await watcher->watch();
+    if (prop)
+    {
+        LOG_INFO("SPDM provisioning completed successfully");
+        co_await tryConnect(ioc, ip, port, controller);
+    }
+    else
+    {
+        LOG_ERROR("SPDM provisioning failed");
+    }
 }
 
 int main(int argc, const char* argv[])
 {
-    auto [conf] = getArgs(parseCommandline(argc, argv), "--conf,-c");
-    net::io_context io_context;
-    ssl::context ssl_context(ssl::context::sslv23_server);
-    if (!conf)
+    try
     {
-        LOG_ERROR(
-            "Configuration file not provided eg: provisioningd --conf /path/to/conf");
+        auto& logger = getLogger();
+        logger.setLogLevel(LogLevel::DEBUG);
+        auto [conf, start] =
+            getArgs(parseCommandline(argc, argv), "--conf,-c", "--start,-s");
+        net::io_context io_context;
+        ssl::context ssl_context(ssl::context::sslv23_server);
+        if (!conf)
+        {
+            LOG_ERROR(
+                "Configuration file not provided eg: provisioningd --conf /path/to/conf");
+            return 1;
+        }
+        std::ifstream confFile(conf.value().data());
+        auto confJson = nlohmann::json::parse(confFile);
+        auto rport = confJson.value("rport", 8090);
+        auto sport = confJson.value("port", 8091);
+        auto ip = confJson.value("rip", std::string{"127.0.0.1"});
+        // Load server certificate and private key
+        ssl_context.set_options(boost::asio::ssl::context::default_workarounds |
+                                boost::asio::ssl::context::no_sslv2 |
+                                boost::asio::ssl::context::single_dh_use);
+
+        ssl_context.use_certificate_chain_file(ENTITY_SERVER_CERT_PATH());
+        ssl_context.use_private_key_file(SERVER_PKEY_PATH(),
+                                         boost::asio::ssl::context::pem);
+        ssl_context.load_verify_file(trusStorePath());
+        ssl_context.set_verify_mode(boost::asio::ssl::verify_peer);
+        BmcWatcher watcher(io_context, ssl_context, sport);
+
+        watcher.onConnectionChange([&](bool connected) {
+            if (connected)
+            {
+                LOG_INFO("BMC watcher connected");
+            }
+            else
+            {
+                LOG_ERROR("BMC watcher disconnected");
+            }
+        });
+        auto conn = std::make_shared<sdbusplus::asio::connection>(io_context);
+        ProvisioningController controller(io_context, conn);
+        conn->request_name(ProvisioningController::busName);
+        controller.setProvisioningStateHandler([&]() {
+            LOG_INFO("Provisioning started");
+            auto watcherPtr = std::make_shared<SpdmWatcher>(conn, "device1");
+            net::co_spawn(io_context,
+                          std::bind_front(startSpdm, std::ref(*conn),
+                                          watcherPtr, std::ref(io_context), ip,
+                                          rport, std::ref(controller)),
+                          net::detached);
+        });
+        if (start)
+        {
+            LOG_INFO("Starting provisioning process");
+            auto watcherPtr = std::make_shared<SpdmWatcher>(conn, "device1");
+            net::co_spawn(io_context,
+                          std::bind_front(startSpdm, std::ref(*conn),
+                                          watcherPtr, std::ref(io_context), ip,
+                                          rport, std::ref(controller)),
+                          net::detached);
+        }
+        io_context.run();
+    }
+    catch (const std::exception& e)
+    {
+        LOG_ERROR("Exception: {}", e.what());
         return 1;
     }
-    std::ifstream confFile(conf.value().data());
-    auto confJson = nlohmann::json::parse(confFile);
-    auto rport = confJson.value("rport", 8090);
-    auto sport = confJson.value("port", 8091);
-    auto ip = confJson.value("rip", std::string{"127.0.0.1"});
-    // Load server certificate and private key
-    ssl_context.set_options(boost::asio::ssl::context::default_workarounds |
-                            boost::asio::ssl::context::no_sslv2 |
-                            boost::asio::ssl::context::single_dh_use);
-
-    ssl_context.use_certificate_chain_file(ENTITY_SERVER_CERT_PATH());
-    ssl_context.use_private_key_file(SERVER_PKEY_PATH(),
-                                     boost::asio::ssl::context::pem);
-    ssl_context.load_verify_file(trusStorePath());
-    ssl_context.set_verify_mode(boost::asio::ssl::verify_peer);
-    BmcWatcher watcher(io_context, ssl_context, sport);
-
-    watcher.onConnectionChange([&](bool connected) {
-        if (connected)
-        {
-            LOG_INFO("BMC watcher connected");
-        }
-        else
-        {
-            LOG_ERROR("BMC watcher disconnected");
-        }
-    });
-    auto conn = std::make_shared<sdbusplus::asio::connection>(io_context);
-    ProvisioningController controller(io_context, conn);
-    conn->request_name(ProvisioningController::busName);
-    controller.setProvisioningStateHandler([&]() {
-        LOG_INFO("Provisioning started");
-        net::co_spawn(
-            io_context,
-            std::bind_front(startSpdm, std::ref(*conn), std::ref(io_context),
-                            ip, rport, std::ref(controller)),
-            net::detached);
-    });
-    io_context.run();
 }
---
+++ src/spdmwatcher.hpp
@@ -0,0 +1,150 @@
+#pragma once
+#include <sdbus_calls.hpp>
+
+#include <string>
+static constexpr auto SPDM_SVC = "xyz.openbmc_project.spdm";
+static constexpr auto SPDM_PATH = "/xyz/openbmc_project/spdm/device1";
+static constexpr auto SPDM_INTF = "xyz.openbmc_project.SpdmDevice";
+static constexpr auto SPDM_PROP = "Status";
+struct SpdmWatcher
+{
+    std::shared_ptr<sdbusplus::asio::connection> conn;
+    std::optional<sdbusplus::bus::match::match> match;
+    std::string id;
+    using SPDM_WATCHER_HANDLER = std::function<void(bool)>;
+    SPDM_WATCHER_HANDLER spdmWatcherHandler;
+
+    SpdmWatcher(std::shared_ptr<sdbusplus::asio::connection> conn,
+                const std::string& id) : conn(conn), id(id)
+    {
+        std::string matchRule = sdbusplus::bus::match::rules::propertiesChanged(
+            SPDM_PATH, SPDM_INTF);
+        match.emplace(
+            *conn, matchRule,
+            std::bind_front(&SpdmWatcher::handlePropertyChange, this));
+    }
+    void handlePropertyChange(sdbusplus::message_t& msg)
+    {
+        std::string interfaceName;
+        std::map<std::string, std::variant<bool>> changedProperties;
+        std::vector<std::string> invalidatedProperties;
+
+        msg.read(interfaceName, changedProperties, invalidatedProperties);
+
+        LOG_INFO("Properties changed on interface: {}", interfaceName);
+
+        changedProperties | std::ranges::views::filter([&](const auto& p) {
+            return p.first == SPDM_PROP;
+        });
+        if (changedProperties.empty())
+        {
+            LOG_ERROR("Property {} not found in changed properties", SPDM_PROP);
+            spdmWatcherHandler(false);
+            return;
+        }
+        auto it = changedProperties.begin();
+        if (!std::holds_alternative<bool>(it->second))
+        {
+            LOG_ERROR("Property {} is not of type string", SPDM_PROP);
+            spdmWatcherHandler(false);
+            return;
+        }
+        auto result = std::get<bool>(it->second);
+        LOG_DEBUG("Property {} changed: {}", SPDM_PROP, result);
+
+        spdmWatcherHandler(result);
+    }
+    void setSpdmWatcherHandler(SPDM_WATCHER_HANDLER handler)
+    {
+        spdmWatcherHandler = std::move(handler);
+    }
+    net::awaitable<bool> watch()
+    {
+        if (!match)
+        {
+            LOG_ERROR("Match object is not initialized");
+            co_return false;
+        }
+        bool result = false;
+        auto h = make_awaitable_handler<bool>([this, &result](auto promise) {
+            auto promise_ptr =
+                std::make_shared<decltype(promise)>(std::move(promise));
+            spdmWatcherHandler = [promise_ptr, &result](bool status) {
+                result = status;
+                promise_ptr->setValues(boost::system::error_code{}, result);
+            };
+        });
+        auto [ec, res] = co_await h();
+        if (ec)
+        {
+            LOG_ERROR("Error in watching SPDM property: {}", ec.message());
+            co_return false;
+        }
+        co_return res;
+    }
+};
+
+using PropertyWatchType = bool;
+inline AwaitableResult<PropertyWatchType,
+                       std::shared_ptr<sdbusplus::bus::match::match>>
+    propertyWatch(sdbusplus::asio::connection& conn, const std::string& path,
+                  const std::string& interface, const std::string& property)
+{
+    std::shared_ptr<sdbusplus::bus::match::match> match;
+    std::string matchRule =
+        sdbusplus::bus::match::rules::propertiesChanged(path, interface);
+    auto h = make_awaitable_handler<PropertyWatchType>([matchRule, property,
+                                                        &match,
+                                                        &conn](auto promise) {
+        auto promise_ptr =
+            std::make_shared<decltype(promise)>(std::move(promise));
+        auto propcallback = [property,
+                             promise_ptr](sdbusplus::message::message& msg) {
+            std::string interfaceName;
+            std::map<std::string, std::variant<PropertyWatchType>>
+                changedProperties;
+            std::vector<std::string> invalidatedProperties;
+
+            msg.read(interfaceName, changedProperties, invalidatedProperties);
+
+            LOG_INFO("Properties changed on interface: {}", interfaceName);
+
+            changedProperties | std::ranges::views::filter([&](const auto& p) {
+                return p.first == property;
+            });
+            if (changedProperties.empty())
+            {
+                LOG_ERROR("Property {} not found in changed properties",
+                          property);
+                promise_ptr->setValues(
+                    boost::system::error_code{
+                        boost::system::errc::make_error_code(
+                            boost::system::errc::no_such_file_or_directory)},
+                    PropertyWatchType{});
+                return;
+            }
+            auto it = changedProperties.begin();
+            if (!std::holds_alternative<PropertyWatchType>(it->second))
+            {
+                LOG_ERROR("Property {} is not of type string", property);
+                promise_ptr->setValues(
+                    boost::system::error_code{
+                        boost::system::errc::make_error_code(
+                            boost::system::errc::invalid_argument)},
+                    PropertyWatchType{});
+                return;
+            }
+            auto result = std::get<PropertyWatchType>(it->second);
+            LOG_DEBUG("Property {} changed: {}", property, result);
+
+            promise_ptr->setValues(boost::system::error_code{},
+                                   std::move(result));
+        };
+        match = std::make_shared<sdbusplus::bus::match::match>(
+            conn, matchRule, std::move(propcallback));
+    });
+    auto [ec, prop] = co_await h();
+    co_return std::make_tuple(
+        ec, prop,
+        std::move(match)); // Return the property value and match object
+}
